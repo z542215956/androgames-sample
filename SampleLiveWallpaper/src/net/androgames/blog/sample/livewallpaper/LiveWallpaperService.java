d35b5de274820a4538880982deefb09e84052406
antoine.vianey <antoine.vianey@2d06d06c-c9e5-11de-a70d-3b9dd4243769>
2010-01-12 12:42:57 +0000

    Initial commit.

---
+++ SampleLiveWallpaper/src/net/androgames/blog/sample/livewallpaper/LiveWallpaperService.java
@@ -0,0 +1,17 @@
+package net.androgames.blog.sample.livewallpaper;
+
+import android.service.wallpaper.WallpaperService;
+
+public class LiveWallpaperService extends WallpaperService {
+
+	@Override
+	public Engine onCreateEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public class SampleEngine extends Engine {
+
+	}
+	
+}
