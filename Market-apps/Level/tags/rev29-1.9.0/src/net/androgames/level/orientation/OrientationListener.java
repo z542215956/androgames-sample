4468044c5914656b862bb2dac4327a78f00050e0
antoine.vianey@gmail.com <antoine.vianey@gmail.com@2d06d06c-c9e5-11de-a70d-3b9dd4243769>
2012-02-15 17:01:30 +0000

    Android Market version 1.9.0

---
+++ Market-apps/Level/tags/rev29-1.9.0/src/net/androgames/level/orientation/OrientationListener.java
@@ -0,0 +1,20 @@
+package net.androgames.level.orientation;
+
+/**
+ * 
+ * A Bubble level for Android phones
+ * 
+ * Under GPL v3 : http://www.gnu.org/licenses/gpl-3.0.html
+ * 
+ * @author antoine vianey
+ *
+ */
+public interface OrientationListener {
+
+	public void onOrientationChanged(Orientation orientation, float pitch, float roll);
+	
+	public void onCalibrationSaved(boolean success);
+	
+	public void onCalibrationReset(boolean success);
+	
+}
